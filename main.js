f8dcb877b0bf44bb34cdb2bc222c4949841fe8a8
Bogdan <86200446+Bogdanctx@users.noreply.github.com>
2022-01-14 09:26:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     $(".elements").animate({
         top: "60%",
         opacity: 1
-    }, 1500);
+    }, 1200);
     setTimeout(() => {
         $("#background-img").animate({
             opacity: 1
         }, 1000);
-    }, 1600);
+    }, 1300);
 });
