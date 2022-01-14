752e9eaeb2d6d2ae3d7f80c2ade4e46bce7e0103
Bogdan <bobotrifan09@gmail.com>
2022-01-14 09:41:08 +0200

    -

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $(".elements").animate({
-        top: "60%",
         opacity: 1
-    }, 1200);
-    setTimeout(() => {
-        $("#background-img").animate({
-            opacity: 1
-        }, 1000);
-    }, 1300);
+    }, 800);
+    $("#background-img").animate({
+        opacity: 1
+    }, 800);
 });
