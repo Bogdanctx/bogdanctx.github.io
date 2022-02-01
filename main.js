444e6786ac4c0ab010dd547ffb37ed21a64cfdb8
Bogdan <bobotrifan09@gmail.com>
2022-02-01 22:03:44 +0200

    =

--- main.js
+++ main.js
@@ -2,7 +2,41 @@ $(document).ready(function() {
     $(".elements").animate({
         opacity: 1
     }, 800);
-    $("#background-img").animate({
+    $("#avatar").animate({
         opacity: 1
-    }, 800);
+    }, 800); 
 });
+
+let r=255, g=255, b=255;
+let increase=0;
+
+function Rainbow() {
+    $("#avatar").css("border", "8px solid rgb("+r+","+g+","+b+")");
+   
+    if(!increase)
+    {
+        if(r>0)
+            r--;
+        else if(g>0)
+            g--;
+        else if(b>0)
+            b--;
+
+        if(b==0)
+            increase=1;
+    }
+    else
+    {
+        if(r<255)
+            r++;
+        else if(g<255)
+            g++;
+        else if(b<255)
+            b++;
+
+        if(b==255)
+            increase=0;
+    }
+}
+
+setInterval(Rainbow, 10);
